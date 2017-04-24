853541eb697278c65156bb66f9bb56ec87ea2338
Thinh P. Tran <trphthinh@gmail.com>
2017-04-24 10:35:45 +0700

    polish.

--- src/clj/directionalsurvey/schema.clj
+++ src/clj/directionalsurvey/schema.clj
@@ -6,16 +6,9 @@
             [directionalsurvey.db :as db]))
 
 (def star-wars-schema
-  (-> (io/resource "star-wars-schema.edn")
+  (-> (io/resource "directionalsurveyschema.edn")
       slurp
       edn/read-string
-      (attach-resolvers {:resolve-hero           db/resolve-hero
-                         :resolve-human          db/resolve-human
-                         :resolve-humans         db/resolve-humans
-                         :resolve-droid          db/resolve-droid
-                         :resolve-droids         db/resolve-droids
-                         :resolve-friends        db/resolve-friends
-                         :resolve-users          db/resolve-users
-                         :resolve-actions        db/resolve-actions
-                         :resolve-mutate-human   db/resolve-mutate-human})
+      (attach-resolvers {:resolve-users          db/resolve-users
+                         :resolve-actions        db/resolve-actions})
       schema/compile))
